27a9f134ce9393c63395f23041032dad14014c8d
Rodrup9 <102837219+Rodrup9@users.noreply.github.com>
2023-10-06 18:38:55 -0600

    Update practica git.py

--- practica git.py
+++ practica git.py
@@ -1,5 +1,12 @@
 print("Hola mundo")
 
 print ("Miguel Fernando San Sebastian")
-print("David Jorge Reyes Guzmán")
+print("David")
 print("Ivan Rodriguez Hernandez")
+
+
+print("Rodrigo Alvarez León")
+
+sebas = 90
+if(sebas > 58)
+  print("Señor mayor")
